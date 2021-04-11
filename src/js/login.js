bb495b1f5a86da6600af87256b8ebaf6e1952bd4
YanMCoutinho <yan.m.coutinho@gmail.com>
2021-04-11 17:46:59 -0300

    finished new login/register modal

---
+++ src/js/login.js
@@ -0,0 +1,22 @@
+const BoxLoginRegister = document.querySelector("#box-login-register");
+const formLogin = document.querySelector("#login");
+const formRegister = document.querySelector("#register");
+
+
+function login() {
+    BoxLoginRegister.classList.add("on")
+    formLogin.classList.add("on")
+    document.getElementById("container-page-landing").style.filter = "blur(1px)";
+}
+
+function toClose() {
+    BoxLoginRegister.classList.remove("on")
+    formLogin.classList.remove("on")
+    formRegister.classList.remove("on")
+    document.getElementById("container-page-landing").style.filter = "blur(0px)";
+}
+
+function register() {
+    formLogin.classList.toggle("on")
+    formRegister.classList.toggle("on")
+}
